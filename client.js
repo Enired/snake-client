a0bc813bc9f8b8bd347fb8f59548c5c1cc9173ef
Derine Banis <d.banis.13@gmail.com>
2022-06-08 13:54:10 -0700

    Implemented adding name to snake/board

--- client.js
+++ client.js
@@ -2,10 +2,14 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
+  // interpret incoming data as text
+  
   const conn = net.createConnection({
     host: "165.227.47.243",// IP address here,
     port: "50541" // PORT number here,
   });
+  
+  conn.setEncoding("utf8");
 
   conn.on('data', () => {
     console.log(`-----\nYOU HAVE BEEN IDLE FOR TOO LONG. YOU ARE NOW DISCONNECTED.\n-----`)
@@ -13,9 +17,12 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log('-----\nYOU HAVE CONNECTED\n-----')
+    conn.write('Name: DTB')
   })
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+
+  // conn.on('connect', () => {
+    
+  // })
 
   return conn;
 };
